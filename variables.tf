d147e21fa797ca7205f00df661b86695b2727248
o-grigorev <oleg_grigorev@mckinsey.com>
2024-01-03 10:05:51 +0100

    remove default any for cluster_endpoint_public_access_cidrs

--- variables.tf
+++ variables.tf
@@ -126,7 +126,6 @@ variable "cluster_endpoint_private_access" {
 variable "cluster_endpoint_public_access_cidrs" {
   description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint"
   type        = list(string)
-  default     = ["0.0.0.0/0"]
 }
 
 #-------------------------------
